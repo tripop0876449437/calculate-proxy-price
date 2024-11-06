c9b731199a6a10f8880812745b59cae78ee0e588
tripop0876449437 <tripop0876449437@gmail.com>
2024-11-06 13:43:22 +0700

    updated theoadv

---
+++ scripts4.js
@@ -0,0 +1,38 @@
+function calculateDay() {
+    let textTheoadv = document.getElementById("textTheoadv").value;
+    let theoadv1 = document.getElementById("theoadv1").value;
+    
+    // console.log(`สวัสดีครับ แจ้งเรื่องโดเมน ${textTheoadv} หมดอายุวันที่ ${theoadv1} กรุณาแจ้งชำระเงินเพื่อต่ออายุ`);
+
+    let domains = textTheoadv.split('\n');
+    let date = [];
+
+    date += ` <p id="Theoadv1Title" class="text-md sm:text-lg font-semibold mb-3">แจ้งต่ออายุโดเมน</p>`
+    domains.forEach(element => {
+        date += `<p>สวัสดีครับ แจ้งเรื่องโดเมน ${element.trim()} หมดอายุวันที่ ${theoadv1} กรุณาแจ้งชำระเงินเพื่อต่ออายุ</p><br>`
+    });
+
+    document.getElementById('totalDataTheoadv1').innerHTML = date;
+
+}
+
+function copyToClipboard(id) {
+    let text = document.getElementById('totalDataTheoadv1');
+    let textElements = text.querySelectorAll('p');
+    let textToCopy = '';
+
+    textElements.forEach((elem) => {
+        textToCopy += elem.innerText + '\n' +'\n';
+    });
+
+    // Use the Clipboard API to copy the text
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        alert('ข้อมูลยอดที่ต้องชำระถูกคัดลอกไปยังคลิปบอร์ด');
+    }, (err) => {
+        console.error('Error copying text to clipboard: ', err);
+    });
+}
+
+function copyToTheo() {
+    copyToClipboard("Theoadv1Text");
+}
---
+++ scriptsRSP.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const menu = document.getElementById("sideMenu");
+    menu.classList.toggle("hidden");
+}
